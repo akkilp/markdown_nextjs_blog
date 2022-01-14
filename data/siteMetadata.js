3438e1cb9aea19300a7b4d805f3b276db9ba7973
Aleksi Kilponen <akkilp@utu.fi>
2022-01-14 14:29:17 +0200

    social card change

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/logo.png',
   email: 'kristiankilponen@gmail.com',
   github: 'https://github.com/akkilp',
   twitter: 'https://twitter.com/Twitter',
